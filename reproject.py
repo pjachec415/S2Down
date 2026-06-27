31c61f0bd3fed460ed02815b711b23356b2a9dbc
Payton Jachec <harrisonjachec@usf.edu>
2026-06-27 12:25:57 -0400

    Update reproject.py

--- reproject.py
+++ reproject.py
@@ -1,14 +1,22 @@
+####################################################
+# clean_tiles.py # Cleans up unfinished tiles.     #
+# ------------------------------------------------ #
+# (c) Payton Jachec 2026. | harrisonjachec@usf.edu #
+# Disclaimer: For research purposes only, not for  #
+# clinical use.                                    #
+####################################################
+
 import rasterio
 from rasterio.warp import reproject, Resampling
 import numpy as np
 
-with rasterio.open("/work_bgfs/h/harrisonjachec/syphilis_hc/indices/NDVI.tif") as ref:
+with rasterio.open("/PATH/TO/IMAGE/TO/PULL/FORMAT/FROM.tif") as ref:
     ref_profile = ref.profile.copy()
     ref_transform = ref.transform
     ref_crs = ref.crs
     ref_shape = (ref.height, ref.width)
 
-with rasterio.open("/work_bgfs/h/harrisonjachec/syphilis_hc/DEM.tif") as src:
+with rasterio.open("/PATH/TO/FILE/TO/REPROJECT.tif") as src:
     dem_resampled = np.empty(ref_shape, dtype=np.float32)
     reproject(
         source=rasterio.band(src, 1),
@@ -22,5 +30,5 @@ with rasterio.open("/work_bgfs/h/harrisonjachec/syphilis_hc/DEM.tif") as src:
 
 out_profile = ref_profile.copy()
 out_profile.update(dtype="float32", count=1, nodata=-9999)
-with rasterio.open("/work_bgfs/h/harrisonjachec/syphilis_hc/DEM_resampled.tif", "w", **out_profile) as dst:
+with rasterio.open("/PATH/TO/OUTPUT/FILE.tif", "w", **out_profile) as dst:
     dst.write(dem_resampled, 1)
